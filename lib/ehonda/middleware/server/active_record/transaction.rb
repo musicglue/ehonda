001c7da313395238c57076cbfaf028a37545d106
Lee Henson <lee.m.henson@gmail.com>
2015-01-08 11:21:55 +0000

    make missing callback error a bit clearer for the developer

--- lib/ehonda/middleware/server/active_record/transaction.rb
+++ lib/ehonda/middleware/server/active_record/transaction.rb
@@ -26,7 +26,19 @@ module Ehonda
           def retrier_options worker
             options = @retrier_default_options.dup
             on_error = options.delete :on_retriable_error
-            options[:exception_cb] = worker.method(on_error) unless on_error.to_s.empty?
+
+            unless on_error.to_s.empty?
+              begin
+                callback_method = worker.method on_error
+              rescue NameError
+                fail "Worker #{worker.class} is using middleware "\
+                     "#{self.class} which expected to find a callback "\
+                     "method called '#{on_error}', but none was found."
+              end
+
+              options[:exception_cb] = callback_method
+            end
+
             options
           end
         end
