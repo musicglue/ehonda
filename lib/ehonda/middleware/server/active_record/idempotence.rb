c62b7168decf98055206e2620b9c041f08c4a9ba
Lee Henson <lee.m.henson@gmail.com>
2015-01-08 10:05:37 +0000

    make sure we store the original version of the processed message

--- lib/ehonda/middleware/server/active_record/idempotence.rb
+++ lib/ehonda/middleware/server/active_record/idempotence.rb
@@ -9,6 +9,7 @@ module Ehonda
 
           def call _worker, queue, sqs_msg, body
             message = TypedMessage.new sqs_msg
+            original_message_body = body.dup
 
             if ProcessedMessage.exists? message_id: message.id, queue: queue
               @logger.info middleware: 'idempotence', ignored_message_id: message.id
@@ -19,7 +20,7 @@ module Ehonda
                 sqs_id: sqs_msg.message_id,
                 message_id: message.id,
                 queue: queue,
-                message: body)
+                message: original_message_body)
             end
           end
         end
